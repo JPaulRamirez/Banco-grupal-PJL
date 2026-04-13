f198b8cc7d2b4fe3a918c61e11d5497afe650b94
JPaulRamirez <p.ramirez9991@gmail.com>
2026-04-13 11:29:37 -0300

    Feature/1304/Creando clase cuenta

--- Banco/src/main/java/org/example/Cuenta.java
+++ Banco/src/main/java/org/example/Cuenta.java
@@ -1,15 +1,37 @@
 package org.example;
 
 public class Cuenta {
-    String nombre;
-    String direccion;
-    String tipoCuenta;
-    double saldo;
+    public String nombre;
+    public String direccion;
+    public String tipoCuenta;
+    public double saldo;
 
     public Cuenta(String nombre, String direccion, String tipoCuenta) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.tipoCuenta = tipoCuenta;
-        this.saldo = 0; 
+        this.saldo = 0;
+    }
+
+    public void depositar(double monto) {
+        saldo += monto;
+    }
+
+    public void transferir(Cuenta destino, double monto) {
+        if (this.saldo >= monto) {
+            this.saldo -= monto;
+            destino.saldo += monto;
+            System.out.println("Transferencia realizada");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Direccion: " + direccion);
+        System.out.println("Tipo: " + tipoCuenta);
+        System.out.println("Saldo: $" + saldo);
+        System.out.println("----------------------");
     }
 }
