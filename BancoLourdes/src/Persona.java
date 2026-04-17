4881ff5848c4a1f77b591d390782639eaf66e8b4
BancoLou <lmrodriguez@gylgroup.com>
2026-04-17 08:58:33 -0300

    mi proyecto de banco

---
+++ BancoLourdes/src/Persona.java
@@ -0,0 +1,42 @@
+public class Persona {
+    String nombre;
+    String dni;
+    String domicilio;
+
+    private Persona(Builder builder) {
+        this.nombre = builder.nombre;
+        this.dni = builder.dni;
+        this.domicilio = builder.domicilio;
+    }
+
+    public static class Builder {
+        private String nombre;
+        private String dni;
+        private String domicilio;
+
+        public Builder setNombre(String nombre) {
+            this.nombre = nombre;
+            return this;
+        }
+
+        public Builder setDni(String dni) {
+            this.dni = dni;
+            return this;
+        }
+
+        public Builder setDomicilio(String domicilio) {
+            this.domicilio = domicilio;
+            return this;
+        }
+
+        public Persona build() {
+            return new Persona (this);
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        return nombre + " (DNI: " + dni + " - Domicilio: " + domicilio + ") ";
+    }
+}
